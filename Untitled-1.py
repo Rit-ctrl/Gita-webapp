51556d366899be28b6334bd40209a5cf523ba271
Rit-ctrl <rithic40@gmail.com>
2021-11-19 12:31:14 +0530

    integrated DrQA results

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import gdown
+import os
 # import argparse
 # import code
 # import prettytable
@@ -14,10 +15,10 @@ from sentence_transformers import SentenceTransformer, util
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-
-    url = 'https://drive.google.com/uc?id=1ddcuoAy0z9aDUVuktZZb_Nu_4Di40bRv'
-    # output = '20150428_collected_images.tgz'
-    gdown.download(url, quiet=False)
+    if os.path.exists('text-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz') == False:
+        url = 'https://drive.google.com/uc?id=1ddcuoAy0z9aDUVuktZZb_Nu_4Di40bRv'
+        # otput = '20150428_collected_images.tgz'
+        gdown.download(url, quiet=False)
 
     ranker = retriever.get_class('tfidf')(tfidf_path='text-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz')
 
@@ -25,8 +26,10 @@ def load_model():
     query_encoder = SentenceTransformer('facebook-dpr-question_encoder-single-nq-base')
 
     # questions = pd.read_csv("Gita_QA - Sheet1.csv").Question.unique()
-    text = pd.read_csv("df_gita(asitis).csv").text.to_list()
-    title = pd.read_csv("df_gita(asitis).csv").id.to_list()
+
+    df_verses = pd.read_csv('df_gita(asitis).csv')
+    text = df_verses.text.to_list()
+    title = df_verses.id.to_list()
 
     passages = []
     for a,b in zip(title,text):
@@ -35,14 +38,14 @@ def load_model():
 
     passage_embeddings = passage_encoder.encode(passages)
 
-    return text,passage_embeddings,query_encoder,ranker
+    return df_verses,text,passage_embeddings,query_encoder,ranker
 
     
 st.title("GITA QUESTION AND ANSWER")
     
 st.header("GITA WEBAPP")
 
-text,passage_embeddings, query_encoder, ranker = load_model()
+df_verses, text, passage_embeddings, query_encoder, ranker = load_model()
 
 def display(text="Button clicked"):
     st.text(text)
@@ -65,9 +68,20 @@ if (st.session_state.submitted):
 
     indices = sorted(range(len(scores[0])), key=lambda i: scores[0][i], reverse=True)[:5]
 
-    st.text(text[indices[0]]+"\n"+text[indices[1]]+"\n"+text[indices[2]]+"\n"+text[indices[3]]+
-    "\n"+text[indices[4]])
+    st.markdown(text[indices[0]])
+    st.markdown("\n"+text[indices[1]])
+    st.markdown("\n"+text[indices[2]])
+    st.markdown("\n"+text[indices[3]])
+    st.markdown("\n"+text[indices[4]])
 
-    st.text(doc_names)
+    # st.markdown(doc_names)
 
+    # for idx, verse in enumerate(doc_names):
+    # print(col)
+    
+    doc_names = pd.Series(doc_names).map(df_verses.set_index('id')['text'])
+    doc_names = doc_names.to_list()
+
+    for verse in doc_names:
+        st.markdown("\n"+verse)
     # st.text(scores[0][indices[0]]+" "+scores[0][indices[1]]+" "+scores[0][indices[2]]+" "+scores[0][indices[3]])
