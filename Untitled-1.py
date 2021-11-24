5963531d76f4438dd2928e3aa068c5b5e99497ac
Rithic Kumar <53635318+Rit-ctrl@users.noreply.github.com>
2021-11-24 12:23:29 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -14,8 +14,12 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 from utils import get_unique_N,sort_list
 
-if os.path.isdir('pygaggle') == False:
-        os.system("git clone --recursive https://github.com/castorini/pygaggle.git")
+# if os.path.isdir('pygaggle') == False:
+#         os.system("git clone --recursive https://github.com/castorini/pygaggle.git")
+
+os.system("pip install pygaggle")
+os.system("pip install -U transformers")
+
 
 from pygaggle.rerank.base import Query, Text
 from pygaggle.rerank.transformer import MonoBERTs
